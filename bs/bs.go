335b0821b3515e8ae0bd9037ffffbf1b89cb8809
Jack Carmichael <carmichaeljr@outlook.com>
2025-05-04 01:03:42 -0600

    updated bs

--- bs/bs.go
+++ bs/bs.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+
 	sbbs "github.com/barbell-math/smoothbrain-bs"
 )
 
@@ -15,12 +17,21 @@ func main() {
 		GenericFmtTarget:      true,
 		GenericGenerateTarget: true,
 	})
-	sbbs.RegisterMergegateTarget(sbbs.MergegateTargets{
-		CheckDepsUpdated:     true,
-		CheckReadmeGomarkdoc: true,
-		CheckFmt:             true,
-		CheckUnitTests:       true,
-	})
+
+	sbbs.RegisterTarget(
+		context.Background(),
+		"mergegate",
+		sbbs.TargetAsStage("fmt"),
+		sbbs.GitDiffStage("Fix formatting to get a passing run!", "fmt"),
+		sbbs.TargetAsStage("gomarkdocInstall"),
+		sbbs.TargetAsStage("gomarkdocReadme"),
+		sbbs.GitDiffStage("Readme is out of date", "gomarkdocReadme"),
+		sbbs.TargetAsStage("updateDeps"),
+		sbbs.GitDiffStage("Out of date packages were detected", "updateDeps"),
+		sbbs.TargetAsStage("sqlcInstall"),
+		sbbs.TargetAsStage("generate"),
+		sbbs.TargetAsStage("test"),
+	)
 
 	sbbs.Main("bs")
 }
